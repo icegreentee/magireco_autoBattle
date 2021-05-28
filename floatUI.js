c4d15cb8e854d7ae01cc1346dd04f6d6cd556b1d
segfault-bilibili <segfault_bilibili@qq.com>
2021-05-28 15:16:57 +0800

    修正AP药确认回复点不到的问题

--- floatUI.js
+++ floatUI.js
@@ -1595,9 +1595,11 @@ function algo_init() {
                             click(bound.centerX(), bound.centerY());
                             // wait for confirmation popup
                         } while (!find(string.revive_popup, 2000));
-                        log("点击确认回复");
                         bound = find(string.revive_confirm, true).bounds();
-                        click(bound.centerX(), bound.centerY());
+                        do {
+                            log("点击确认回复");
+                            click(bound.centerX(), bound.centerY());
+                        } while (find(string.revive_popup, 2000));
                         usedrug = true;
                         updateDrugLimit(i);
                         break;
