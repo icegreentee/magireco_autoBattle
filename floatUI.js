8ec6773fd2b03a22574b75d4b396fbc13a25e198
segfault-bilibili <segfault_bilibili@qq.com>
2021-06-15 21:54:10 +0800

    参照find等函数的写法，处理wait设为true或false的情况

--- floatUI.js
+++ floatUI.js
@@ -1472,8 +1472,7 @@ function algo_init() {
 
     //检测AP，缺省wait的情况下只检测一次就退出
     function getAP(wait) {
-        var startTime = 0;
-        if (wait != null) startTime = new Date().getTime();
+        var startTime = new Date().getTime();
 
         if (findID("baseContainer")) {
             // values and seperator are together
@@ -1501,7 +1500,7 @@ function algo_init() {
                 }
                 if (result) return result;
                 sleep(100);
-            } while (wait != null && new Date().getTime() < startTime + wait);
+            } while (wait === true || (wait && new Date().getTime() < startTime + wait));
         } else {
             // ... are seperate
             do {
@@ -1532,7 +1531,7 @@ function algo_init() {
                 }
                 if (result) return result;
                 sleep(100);
-            } while (wait != null && new Date().getTime() < startTime + wait);
+            } while (wait === true || (wait && new Date().getTime() < startTime + wait));
         }
     }
 
