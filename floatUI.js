a8835bc11e52d6a3af655b93a0eb3bbaa9b0c9db
segfault-bilibili <segfault_bilibili@qq.com>
2021-10-15 20:50:15 +0800

    移动clickAerrPopup到检查是否有动作录制数据之后

--- floatUI.js
+++ floatUI.js
@@ -7017,12 +7017,12 @@ function algo_init() {
                     break;
                 }
                 case STATE_LOGIN: {
-                    clickAerrPopup();//先点掉系统的崩溃提示弹窗
                     if (lastOpList == null) {
                         toastLog("没有动作录制数据,退出");
                         stopThread();
                         break;
                     }
+                    clickAerrPopup();//先点掉系统的崩溃提示弹窗
                     if (isGameDead(2000) == "crashed") {
                         state = STATE_CRASHED;
                         break;
