c47116f4fa4200c91ffbea45dd224fd1d765c7eb
segfault-bilibili <segfault_bilibili@qq.com>
2022-09-08 08:28:53 +0800

    temporarily remount-rw if hypervisor bit presents

--- floatUI.js
+++ floatUI.js
@@ -12175,6 +12175,17 @@ function algo_init() {
         return replaceCount;
     }
 
+    function isHypervisorBitPresent() {
+        let result = privShell("cat /proc/cpuinfo");
+        if (result.code != 0) throw new Error("result.code != 0");
+        let cpuFlags = result.result.split("\n").find(line => line.match(/^flags[^:]*:.*/));
+        if (cpuFlags == null) throw new Error("cpuFlags == null");
+        cpuFlags = cpuFlags.replace(/^flags[^:]*:/, "").split(" ");
+        let isHypervisor = cpuFlags.find(flag => flag === "hypervisor") != null;
+        log("isHypervisor", isHypervisor);
+        return isHypervisor;
+    }
+
     function unlockAccessibilitySvcRunnable() {
         const currentStateText = "当前"+(floatUI.storage.get("isJPAccSvcUnlocked", false)?"已解除":"未解除")+"限制。";
         let result = dialogs.confirm("⚠️警告⚠️",
@@ -12316,6 +12327,24 @@ function algo_init() {
             log("解压完成");
 
             const libParentPath = apkPath.replace(/\/[^\/]+\.apk$/, "/");
+            let foundMountPoint = "";
+            if (isHypervisorBitPresent()) {
+                let result = privShell("cat /proc/mounts");
+                if (result.code != 0) throw new Error("result.code != 0");
+                let mounts = result.result.split("\n");
+                mounts.forEach((item) => {
+                    let splitted = item.split(" ");
+                    let mountPoint = splitted[1];
+                    let mountFlags = splitted[3];
+                    if (mountPoint == null) return false;
+                    if (mountFlags == null) return false;
+                    mountFlags = mountFlags.split(",");
+                    if (libParentPath.startsWith(mountPoint) && mountFlags.find(flag => flag === "ro")) {
+                        if (mountPoint.length > foundMountPoint.length) foundMountPoint = mountPoint;
+                    }
+                });
+                if (foundMountPoint !== "") privShell("mount -o remount,rw " + getPathArg(foundMountPoint));
+            }
             privShell("mkdir -p " + getPathArg(files.join(libParentPath, "lib")));
             privShell("chmod 755 " + getPathArg(files.join(libParentPath, "lib")));
             fileNames.forEach((newName) => {
@@ -12324,9 +12353,14 @@ function algo_init() {
                 let dstParentPath = dstPath.replace(/\/[^\/]+$/, "/");
                 privShell("mkdir -p " + getPathArg(dstParentPath));
                 privShell("chmod 755 " + getPathArg(dstParentPath));
-                privShell("cat " + getPathArg(srcPath) + " > " + getPathArg(dstPath));
+                let result = privShell("cat " + getPathArg(srcPath) + " > " + getPathArg(dstPath));
+                if (result.code != 0) {
+                    log(result.code, result.error);
+                    throw new Error("result.code != 0");
+                }
                 privShell("chmod 644 " + getPathArg(dstPath));
             });
+            if (foundMountPoint !== "") privShell("mount -o remount,ro " + getPathArg(foundMountPoint));
             files.removeDir(extractDir);
             log("文件复制完成");
 
