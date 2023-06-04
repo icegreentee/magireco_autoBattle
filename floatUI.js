70e3f44deb28d32e80c4fba420895b8851b27a49
segfault-bilibili <segfault_bilibili@qq.com>
2023-06-04 17:36:06 +0800

    avoid exceeded toast quota

--- floatUI.js
+++ floatUI.js
@@ -13101,10 +13101,14 @@ function algo_init() {
 
         initOCR();
 
-        toastLog("半自动辅助开荒脚本能够在一个章节(section)内自动选BATTLE进行周回；");
-        toastLog("且支持在第一回合自动凑连携（可在设置中关闭）；");
-        toastLog("以及支持地图型周回,但在找不到没打过(new)的关卡时不会自动拖动地图；");
-        toastLog("另外,不会嗑药,也不会处理掉线等情况。");
+        toast("半自动辅助开荒脚本能够在一个章节(section)内自动选BATTLE进行周回；");
+        toast("且支持在第一回合自动凑连携（可在设置中关闭）；");
+        threads.start(function () {
+            // avoid exceeded toast quota
+            sleep(6000);
+            toast("以及支持地图型周回,但在找不到没打过(new)的关卡时不会自动拖动地图；");
+            toast("另外,不会嗑药,也不会处理掉线等情况。");
+        });
 
         log("缩放图片...");
         resizeKnownImgs();//必须放在initialize后面
